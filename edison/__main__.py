7968658205b59ca3fb700b02ad2afd1719bb2810
suyog prasai <suyog224356@gmail.com>
2025-02-09 03:46:20 +0545

    updated code

--- edison/__main__.py
+++ edison/__main__.py
@@ -1,23 +1,5 @@
-from edison.models.Car import Car
-from edison.helpers.DataPacket import DataPacketBuilder
-from edison.helpers.SendPacket import SerialPacketSender
-from edison.components.arduino_control.Control import EdisonCar  # Replace 'your_module' with the actual module name
 
-from edison._lib.path_generator import get_route,save_route_as_geojson
-from edison.components.point_navigation.PointNavigator import PointNavigator
-from edison.components.device_location.DeviceLocation import DeviceLocationReader
-from edison.components.road_traversing_algorithm.Traverser import Traverser
+from edison.components.MainController import MainController
 
-
-from edison._lib.get_video import GetWebcam
-
-car = EdisonCar()
-p = PointNavigator(car)
-
-t = Traverser(
-    car,
-    point_navigator=p
-)
-
-print(t.get_magnitude())
-print(t.turn_angle())
+m = MainController()
+m.run_loop()
